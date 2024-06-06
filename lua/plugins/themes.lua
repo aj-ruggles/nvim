4ee872a58c59047d55bbc0fcbe2196f07cd01883
aj-ruggles <austinruggles@gmail.com>
2024-06-06 08:19:25 -0500

    first commit

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		-- config = function()
+		--   vim.cmd.colorscheme("catppuccin")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				indent = { ensure = true },
+				highlight = { ensure = true },
+			})
+		end,
+	},
+}
